a4dab0ba22735d2c95da42b61453894e67d505af
doantrantuandat <doantrantuandat.98@gmail.com>
2020-02-27 20:11:53 +0700

    modified Apriori.py

--- Project_DataMining_SuperMarket/src/Apriori.py
+++ Project_DataMining_SuperMarket/src/Apriori.py
@@ -11,7 +11,7 @@ store_data = pd.read_csv(path, header=None)
 
 records = []
 for i in range(0, 7501):
-    records.append([str(store_data.values[i,j]) for j in range(0, 20)])
+    records.append([str(store_data.values[i, j]) for j in range(0, 20)])
 
 records_withoutNan = []
 for i in range(0, len(records)):
@@ -20,13 +20,10 @@ for i in range(0, len(records)):
         if str(records[i][j]) != "nan":
             new.append(str(records[i][j]))
     records_withoutNan.append(new)
-#print(records_withoutNan)
-  
- 
-association_rules = apriori(records_withoutNan, min_support=0.0045, min_confidence=0.2, min_lift=3)
-association_results = list(association_rules)
 
-print(association_results)
+association_rules = apriori(
+    records_withoutNan, min_support=0.0045, min_confidence=0.2, min_lift=3)
+association_results = list(association_rules)
 
 
 print("=============================================")
@@ -34,8 +31,8 @@ print("Number association rules: " + str(len(association_results)))
 
 for item in association_results:
     print("=====================================")
-    print("Rule: " + str(list(item[2][0][0])) + " -> " + str(list(item[2][0][1])))
+    print("Rule: " + str(list(item[2][0][0])) +
+          " -> " + str(list(item[2][0][1])))
     print("Support: " + str(item[1]))
     print("Confidence: " + str(item[2][0][2]))
     print("Lift: " + str(item[2][0][3]))
-    
